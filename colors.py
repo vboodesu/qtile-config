77517253fbcfca0c80ee568151c7ec854446cf21
vboodesu <88963915+vboodesu@users.noreply.github.com>
2022-09-12 16:43:22 +0000

    Add files via upload

---
+++ colors.py
@@ -0,0 +1,61 @@
+nord = {
+    'nord0':     '#2e3440',
+    'nord1':     '#3b4252',
+    'nord2':     '#434c54',
+    'nord3':     '#4c566a',
+    'nord4':     '#d8dee9',
+    'nord5':     '#eceff4',
+    'nord6':     '#8fbcbb',
+    'nord7':     '#88c0d0',
+    'nord8':     '#81a1c1',
+    'nord9':     '#5e81ac',
+    'nord10':    '#bf616a',
+    'nord11':    '#d08770',
+    'nord12':    '#ebcb8b',
+    'nord13':    '#a3b38c',
+    'nord14':    '#b48ead'
+}
+
+nord_fox = {
+    'bg': "#2e3440",
+    'fg': "#b9bfca",
+    'fg_gutter': "#4b5668",
+    'black': "#3b4252",
+    'red': "#bf616a",
+    'green': "#a3be8c",
+    'yellow': "#ebcb8b",
+    'blue': "#81a1c1",
+    'magenta': "#b48ead",
+    'cyan': "#88c0d0",
+    'white': "#e5e9f0",
+    'orange': "#c9826b",
+    'pink': "#bf88bc",
+}
+
+gruvbox = {
+    'bg':           '#282828',
+    'fg':           '#d4be98',
+    'dark-red':     '#ea6962',
+    'red':          '#ea6962',
+    'dark-green':   '#a9b665',
+    'green':        '#a9b665',
+    'dark-yellow':  '#e78a4e',
+    'yellow':       '#d8a657',
+    'dark-blue':    '#7daea3',
+    'blue':         '#7daea3',
+    'dark-magenta': '#d3869b',
+    'magenta':      '#d3869b',
+    'dark-cyan':    '#89b482',
+    'cyan':         '#89b482',
+    'dark-gray':    '#665c54',
+    'gray':         '#928374',
+
+    'fg4':          '#766f64',
+    'fg3':          '#665c54',
+    'fg2':          '#504945',
+    'fg1':          '#3c3836',
+    'bg0':          '#32302f',
+    'fg0':          '#1d2021',
+    'fg9':          '#ebdbb2'
+}
+
